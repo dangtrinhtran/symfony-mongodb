b79c4b63174bf138d936e9b317c3c3f2ed5d6367
rony <rony@likipe.se>
2013-08-01 22:43:08 -0700

    Update UploadHandlerService.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
 			new FOS\UserBundle\FOSUserBundle(),
             new Likipe\FrontendBundle\LikipeFrontendBundle(),
             new Likipe\ProductBundle\LikipeProductBundle(),
+			new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
